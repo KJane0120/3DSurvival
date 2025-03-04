e2d4b579a0b109bca8442de0908f60a5ca4be9d3
KJane0120 <rlawoals5755@gmail.com>
2025-03-05 00:31:02 +0900

    [Feat] 낮과 밤 구현
    시간에 따른 낮과 밤 환경을 구현했습니다

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float time; // 0.5일때 정오. 각도는 90도, 시간은 12시라고 생각
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon; //Vector 90, 0, 0
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionIntensityMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = (time + timeRate * Time.deltaTime) % 1.0f; //1.0f로 나누어주는 이유는 0~1 사이의 값으로 만들어주기 위함
+
+        UpdateLighting(sun, sunColor, sunIntensity);
+        UpdateLighting(moon, moonColor, moonIntensity);
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+    void UpdateLighting(Light lightSource, Gradient gradient, AnimationCurve intensityCurve)
+    {
+        float intensity = intensityCurve.Evaluate(time);
+
+        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4f; //최적화? 
+        lightSource.color = gradient.Evaluate(time);
+        lightSource.intensity = intensity;
+
+        GameObject go = lightSource.gameObject;
+        if(lightSource.intensity == 0 && go.activeInHierarchy)
+        {
+            go.SetActive(false);
+        }
+        else if (lightSource.intensity > 0 && !go.activeInHierarchy)
+        {
+            go.SetActive(true);
+        }
+    }
+}
